83549015219947f8d79c63a73190e05ea06a2918
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-03-08 22:04:39 +0530

    edit

--- api/config/api.config.ts
+++ api/config/api.config.ts
@@ -5,7 +5,7 @@
 export const API_CONFIG = {
   // Base URL - Update this with your backend API URL
   BASE_URL: __DEV__
-    ? 'http://192.168.144.170:3000' // Localhost (Dev) - If logic fails, use: 'https://backend.sotercare.com/'
+    ? 'http://192.168.35.170:3000' // Localhost (Dev) - If logic fails, use: 'https://backend.sotercare.com/'
     : 'https://unlikely-caryn-sotercare-873e6112.koyeb.app', // Production
 
   // Request timeout (milliseconds)
